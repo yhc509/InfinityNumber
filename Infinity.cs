083d75b023af0c7a7c7f024e424508f4278c6f35
yhc509 <yhc5091@gmail.com>
2020-01-26 09:56:40 +0900

    Infinity 작업

---
+++ Infinity.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+
+public struct Infinity
+{
+    public static string[] TypeArr = new[]
+    {
+        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
+        "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+    };
+    
+    public class InvalidParameterException : Exception { }
+
+    private int TypeIndex
+    {
+        get { return Math.Abs(Index); }
+    }
+    
+    public int Index { get; private set; }
+    public double Number { get; private set; }
+
+    #region Constucor
+    public Infinity(int input) : this((long) input) { }
+    
+    public Infinity(long input)
+    {
+        this = default(Infinity);
+        Index = GetIndex(input);
+        var t = Math.Pow(1000.0, Index);
+        Number = input / t;
+    }
+    
+    public Infinity(string input) : this()
+    {
+        string n = input.Substring(0, input.Length - 1);
+        string type = input.Substring(input.Length- 1, 1);
+        Index = -1;
+        Index = GetTypeArrFromString(type);
+        Number = double.Parse(n);
+        RecaculateIndex();
+    }
+
+    public Infinity(double input, string type) : this()
+    {
+        Index = -1;
+        Index = GetTypeArrFromString(type);
+        Number = input;
+        RecaculateIndex();
+    }
+
+    public Infinity(double number, int index)
+    {
+        Number = number;
+        Index = index;
+    }
+
+    public Infinity(Infinity origin)
+    {
+        Number = origin.Number;
+        Index = origin.Index;
+    }
+    
+    #endregion
+
+    public override string ToString()
+    {
+        return string.Format("{0:F1}{1}", Number, TypeArr[TypeIndex]);
+    }
+
+
+    #region Implicit
+    public static implicit operator Infinity(int input) {
+        return new Infinity(input);
+    }
+    
+    public static implicit operator Infinity(long input) {
+        return new Infinity(input);
+    }
+    
+    public static implicit operator Infinity(string input) {
+        return new Infinity(input);
+    }
+    #endregion
+    
+    #region Calculate
+
+    public static Infinity operator +(Infinity inf1, int n)
+    {
+        return (inf1 + new Infinity(n));
+    }
+        
+    public static Infinity operator +(Infinity inf1, long n)
+    {
+        return (inf1 + new Infinity(n));
+    }
+    
+    public static Infinity operator +(Infinity inf1, Infinity inf2)
+    {
+        Infinity result = inf1;
+        var subIndex = result.Index - inf2.Index;
+        var t = Math.Pow(1000.0, subIndex);
+        result.Number += inf2.Number / t;
+        
+        return result;
+    }
+    
+    
+    public static Infinity operator -(Infinity inf1, Infinity inf2)
+    {
+        Infinity big, small;
+        if (inf1 > inf2)
+        {
+            big = inf1;
+            small = inf2;
+        }
+        else
+        {
+            big = inf2;
+            small = inf1;
+        }
+
+        var subIndex = big.Index - small.Index;
+        var t = Math.Pow(1000.0, subIndex);
+        big.Number -= small.Number / t;
+        
+        return big;
+    }
+    
+    public static Infinity operator *(Infinity inf1, int n)
+    {
+        return (inf1 * (double) n);
+    }
+    
+    public static Infinity operator *(Infinity inf1, long n)
+    {
+        return (inf1 * (double) n);
+    }
+    
+    public static Infinity operator *(Infinity inf1, float n)
+    {
+        return (inf1 * (double) n);
+    }
+    
+    public static Infinity operator *(Infinity inf1, double n)
+    {
+        Infinity result = inf1;
+        result.Number *= n;
+        result.RecaculateIndex();
+        return result;
+    }
+
+    public static Infinity operator *(Infinity inf1, Infinity inf2)
+    {
+        Infinity result = inf1;
+        result.Number *= inf2.Number;
+        result.Index += inf2.Index;
+        result.RecaculateIndex();
+        return result;
+    }
+
+    
+    public static Infinity operator /(Infinity inf1, int n)
+    {
+        return (inf1 / (double) n);
+    }
+    
+    public static Infinity operator /(Infinity inf1, long n)
+    {
+        return (inf1 / (double) n);
+    }
+    
+    public static Infinity operator /(Infinity inf1, float n)
+    {
+        return (inf1 / (double) n);
+    }
+    
+    public static Infinity operator /(Infinity inf1, double n)
+    {
+        Infinity result = inf1;
+        result.Number /= n;
+        result.RecaculateIndex();
+        return result;
+    }
+    
+    public static Infinity operator /(Infinity inf1, Infinity inf2)
+    {
+        Infinity result = inf1;
+        result.Number /= inf2.Number;
+        result.Index -= inf2.Index;
+        result.RecaculateIndex();
+        return result;
+    }
+
+    #endregion
+    
+    #region Compare
+    public static bool operator==(Infinity inf1, Infinity inf2)
+    {
+        return inf1.Index == inf2.Index &&
+               inf1.Number == inf2.Number;
+    }
+
+    public static bool operator !=(Infinity inf1, Infinity inf2)
+    {
+        return !(inf1 == inf2);
+    }
+
+    public static bool operator>(Infinity inf1, Infinity inf2)
+    {
+        if (inf1.Index == inf2.Index)
+            return inf1.Number > inf2.Number;
+
+        return inf1.Index > inf2.Index;
+
+    }
+
+    public static bool operator <(Infinity inf1, Infinity inf2)
+    {
+        if (inf1.Index == inf2.Index)
+            return inf1.Number < inf2.Number;
+
+        return inf1.Index < inf2.Index;
+    }
+    
+    public bool Equals(Infinity other)
+    {
+        return Index == other.Index && Number.Equals(other.Number);
+    }
+
+    public override bool Equals(object obj)
+    {
+        return obj is Infinity other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        unchecked
+        {
+            return (Index * 397) ^ Number.GetHashCode();
+        }
+    }
+    #endregion
+
+    #region Util
+
+    private void RecaculateIndex()
+    {
+        if (Number >= 1000.0)
+        {
+            Number /= 1000.0;
+            Index++;
+        }
+        else if (Number < 1)
+        {
+            Number *= 1000.0;
+            Index--;
+        }
+    }
+
+    private int GetIndex(long input)
+    {
+        long temp = input;
+        int index = 0;
+        for (index = 0; temp >= 1000; index++)
+        {
+            temp /= 1000;
+        }
+
+        return index;
+    }
+    
+    private int GetTypeArrFromString(string str)
+    {
+        int result = -1;
+        for (int i = 0; i < TypeArr.Length; i++)
+        {
+            if (TypeArr[i] == str)
+            {
+                result = i;
+                break;
+            }
+        }
+
+        if (result == -1)
+            throw new InvalidParameterException();
+        
+        return result;
+    }
+    #endregion
+}
--- README.md
+++ README.md
@@ -1,1 +1,74 @@
 # InfinityNumber
+거의 무한에 근접하는 수를 표현하는 구조체입니다.
+
+
+## 구조
+Infinity는 아래 두 데이터의 조합으로 표현됩니다.
+- 자리수를 나타내는 int형 데이터
+- 값을 나타내는 double형 데이터 (0 ~ 999.9999999.... 의 범위만 갖습니다.)
+
+따라서 표현 가능한 수의 범위는
+대략 **1k * 10^-2,147,483,648 ~ 1k*10^2,147,483,648** 정도가 됩니다.
+
+## 표현
+1000 단위로 자리수를 계산하여 단위를 붙입니다.
+단위는 A,B,C,D, ... 알파벳 순으로 Z까지 할당되어 있으며, 단위를 더 추가할 수도 있습니다.
+단위의 아래값으로는 소수점 한자리까지만 표현합니다.
+
+- 1 => 1.0A
+- 10 => 10.0A
+- 1,000 => 1.0B
+- 1,001 => 1.0B
+- 1,000,000 => 1.0C
+
+## 사용법
+
+### 할당
+아래는 모두 10.0A 를 표현하는 예시입니다.
+```csharp
+Infinity n = 10;
+Infinity n = "10A";
+Infinity n = "10.0A";
+var n = new Infinity(10);
+var n = new Infinity("10A");
+var n = new Infinity("10.0A");
+var n = new Infinity(10.0, "A");
+var n = new Infinity(10.0, 0);
+```
+
+### 지원되는 사칙 연산
+#### 더하기(+)
+- Infinity + int
+- Infinity + long
+- Infinity + Infinity
+
+#### 빼기(-)
+- Infinity - int
+- Infinity - long
+- Infinity - Infinity
+
+#### 곱하기(*)
+- Infinity * int
+- Infinity * long
+- Infinity * float
+- Infinity * double
+- Infinity * Infinity
+
+#### 나누기(/)
+- Infinity / int
+- Infinity / long
+- Infinity / float
+- Infinity / double
+- Infinity / Infinity
+
+### 지원되는 비교 연산
+#### ==, !=
+- Infinity == Infinity
+- Infinity != Infinity
+
+#### >, <
+- Infinity > Infinity
+- Infinity < Infinity
+
+#### Equals
+- Infinity.Equals(Infinity)
